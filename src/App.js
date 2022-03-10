8d2b6464205f4962e2b89b3c06881f5d66aeaaa4
Yanivv77 <Yanivv77@gmail.com>
2022-03-11 01:22:17 +0200

    Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css'
-import Layout from './Components/Layout'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Contact from './Pages/Contact'
+import Projects from './Pages/Projects'
 
 function App() {
   return (
-    <Layout>
-      <h1>das</h1>
-    </Layout>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
---
+++ src/Pages/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../Components/Layout'
+
+function Projects() {
+  return (
+    <Layout>
+      <h1>Projects</h1>
+    </Layout>
+  )
+}
+
+export default Projects
